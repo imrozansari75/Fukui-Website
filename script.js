e1b438d6937ab319a9c64966ee8081ce7238a97c
Imroz Ansari <136307865+imrozansari75@users.noreply.github.com>
2024-08-04 13:41:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// const showMenu = (toggleId, navId) => {
+//   const toggle = document.getElementById(toggleId),
+//     nav = document.getElementById(navId)
+
+//   toggle.addEventListener('click', () => {
+//     // Add show-menu class to nav menu
+//     nav.classList.toggle('show-menu')
+
+//     // Add show-icon to show and hide the menu icon
+//     toggle.classList.toggle('show-icon')
+//   })
+// }
+
+// showMenu('nav-toggle', 'nav-menu')
+
+// const showMenu = (toggleId, navId) => {
+//   const toggle = document.getElementById(toggleId),
+//     nav = document.getElementById(navId),
+//     navLinks = document.querySelectorAll('.nav__link');
+
+//   // Toggle the menu and icon on toggle button click
+//   toggle.addEventListener('click', () => {
+//     nav.classList.toggle('show-menu');
+//     toggle.classList.toggle('show-icon');
+//   });
+
+//   // Close the menu when any navigation link is clicked
+//   navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//       nav.classList.remove('show-menu');
+//       toggle.classList.remove('show-icon');
+//     });
+//   });
+// };
+
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
@@ -21,26 +56,31 @@ showMenu('nav-toggle', 'nav-menu');
 showDropdown('services-toggle', 'services-menu');
 
 
- // JavaScript to handle truncation
- document.addEventListener("DOMContentLoaded", function () {
+// Initialize the menu toggle functionality
+showMenu('nav-toggle', 'nav-menu');
+
+
+// JavaScript to handle truncation
+document.addEventListener("DOMContentLoaded", function () {
   const truncateText = (selector, maxLength) => {
-    const elements = document.querySelectorAll(selector);
-    elements.forEach(element => {
-      const originalText = element.textContent;
-      if (originalText.length > maxLength) {
-        const truncatedText = originalText.substring(0, maxLength) + '...';
-        element.textContent = truncatedText;
-      }
-    });
+      const elements = document.querySelectorAll(selector);
+      elements.forEach(element => {
+          const originalText = element.textContent;
+          if (originalText.length > maxLength) {
+              const truncatedText = originalText.substring(0, maxLength) + '...';
+              element.textContent = truncatedText;
+          }
+      });
   };
 
   truncateText('#accounting-text', 200);
-  truncateText('#hrm-text', 250);
+  truncateText('#hrm-text', 200);
   truncateText('#admin-text', 200);
-  truncateText('#it-text', 300);
+  truncateText('#it-text', 200);
   truncateText('#compliance-text', 200);
   truncateText('#scm-text', 200);
 });
+
 
 function toggleCard(card) {
   const content = card.nextElementSibling;
